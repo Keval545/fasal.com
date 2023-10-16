d640696c40447de875b33de3cd45c9c451cd61da
Keval545 <108585269+Keval545@users.noreply.github.com>
2023-10-17 02:32:58 +0530

    map work

--- frontend/src/components/SideBar/SideBarComponents.jsx
+++ frontend/src/components/SideBar/SideBarComponents.jsx
@@ -38,11 +38,6 @@ const SideBarComponents = () => {
                     <i className="fas icon-fas fa-seedling"></i>Harvest
                     </ListGroup.Item>
                 </LinkContainer>
-                <LinkContainer className='link-contain' to='/admin/map'>
-                    <ListGroup.Item className="border-0 item">
-                        <i className="fas icon-fas fa-map-marker-alt"></i>Map
-                    </ListGroup.Item>
-                </LinkContainer>
             </ListGroup>
         </div>
     )
